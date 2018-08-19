23ad987fa933da2094a91164dab80916a7cd4aa8
aniljrajpurohit <anilrajpurohit23@gmail.com>
2018-08-19 10:12:47 +0530

    1st commit

---
+++ LED_7_Segment.ino
@@ -0,0 +1,99 @@
+#include <SPI.h>
+#define   LOAD  10
+#define   MOSI  11
+#define   MISO  12
+#define   SCK   13
+#define   SERIAL_DEBUG
+//#define USE_BLINKWITHOUTDELAY
+#ifdef USE_BLINKWITHOUTDELAY
+//for timing purposes
+unsigned long previousMillis = 0;        // will store last time LED was updated
+const long interval = 500;           // interval at which to blink (milliseconds)
+#endif
+void disp(uint8_t, uint8_t);
+void setup() {
+  //  pinMode(A0, OUTPUT);
+  //  pinMode(A2, OUTPUT);
+  //  digitalWrite(A0, HIGH);
+  //  digitalWrite(A2, LOW);
+
+#ifdef SERIAL_DEBUG
+  Serial.begin(9600);
+  Serial.println("HEllo");
+#endif
+
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV2);
+
+  pinMode(LOAD, OUTPUT);
+  pinMode(MOSI, OUTPUT);
+  pinMode(SCK, OUTPUT);
+  pinMode(MISO, INPUT);
+  digitalWrite(LOAD, HIGH);
+  //  Serial.begin(9600);
+}
+uint8_t dg[] {
+  0b00111111,//0
+  0b00000110,//1
+  0b01011011,//2
+  0b01001111,//3
+  0b01100110,//4
+  0b01101101,//5
+  0b01111101,//6
+  0b00000111,//7
+  0b01111111,//8
+  0b01101111,//9
+  //pgfedbca
+};
+
+uint8_t select[] {
+  0b00000001,
+  0b00000010,
+  0b00000100,
+  0b00001000,
+  0b00010000,
+  0b00100000,
+  0b01000000,
+  0b10000000,
+};
+
+int dataArray[] = {1,2,3,4,5,6,7,8};
+
+//void dataToArray(uint32_t data) {
+//  if (data > 99999999) {
+//    data%=100000000;
+//  }
+//  dataArray[]
+//  dataArray[]
+//  dataArray[]
+//  dataArray[]
+//  dataArray[]
+//  dataArray[]
+//  dataArray[]
+//  dataArray[]
+//}
+int counter = 0;
+int data;
+uint16_t selector = 1;
+
+
+void loop() {
+  //  Serial.println(selector);
+  disp(dataArray[counter], selector); //0b10000000
+  selector <<= 1;
+  counter++;
+  counter %= 8;
+
+  selector %= 255;
+  //  data = analogRead(A1);
+  //  //  Serial.println(data);
+  //dataToArray(12345678);
+  delay(1);
+}
+void disp(uint8_t digit = 0, uint8_t place = 0) {
+  digitalWrite(LOAD, LOW);
+  SPI.setBitOrder(MSBFIRST);
+  SPI.transfer(place);//transfer place
+  SPI.transfer(dg[digit]);//transfer digit
+  digitalWrite(LOAD, HIGH);
+}
