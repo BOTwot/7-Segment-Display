64b2eb21e6fe6adf07432154182c8b937ae33478
aniljrajpurohit <anilrajpurohit23@gmail.com>
2018-08-21 17:20:48 +0530

    Added code for Float Display

--- LED_7_Segment_Master.ino
+++ LED_7_Segment_Master.ino
@@ -1,17 +1,21 @@
 #include <I2C_Anything.h>
 #include<Wire.h>
-
+float voltage = 0;
 const int slaveAddress = 5;
 void setup() {
   Wire.begin();
+  pinMode(A1, INPUT);
   //  Serial.begin(9600);
 }
-
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 void loop() {
-  uint32_t i = 0;
-  for (i = 0; i < 10000000; i++) {
-    Wire.beginTransmission(slaveAddress);
-    I2C_writeAnything (i);
-    Wire.endTransmission();
-  }
+  float reading = analogRead(A1);
+  voltage = mapfloat(reading, 0, 1023, 0, 12);
+  Wire.beginTransmission(slaveAddress);
+  I2C_writeAnything (voltage);
+  Wire.endTransmission();
+
 }
--- LED_7_Segment_Slave.ino
+++ LED_7_Segment_Slave.ino
@@ -8,8 +8,9 @@
 #define   SCK   13
 //#define   SERIAL_DEBUG
 
+float decimal = 0;
 void disp(uint8_t, uint8_t);
-uint32_t i2c_data = 0;
+uint32_t integer = 0;
 
 void setup() {
   Wire.begin(5);
@@ -18,7 +19,6 @@ void setup() {
   Serial.begin(9600);
   Serial.println("HEllo");
 #endif
-
   SPI.begin();
   SPI.setClockDivider(SPI_CLOCK_DIV2);
 
@@ -43,14 +43,40 @@ uint8_t dg[] {
   //pgfedbca
 };
 
+uint8_t dg1[] {
+  0b10111111,//0.
+  0b10000110,//1.
+  0b11011011,//2.
+  0b11001111,//3.
+  0b11100110,//4.
+  0b11101101,//5.
+  0b11111101,//6.
+  0b10000111,//7.
+  0b11111111,//8.
+  0b11101111,//9.
+  0b10000000//DP
+  //pgfedbca
+};
+
 int dataArray[] = {0, 0, 0, 0, 0, 0, 0, 0};
-void dataToArray(uint32_t data) {       //Splits the digits and feeds into dataArray
-  for (int i = 0; i <= 7; i++) {
-    if (data <= 0)
+void dataToArray(uint32_t decimal, uint32_t integer) {      //Splits the digits and feeds into dataArray
+  int i = 0;
+  int j = 4;
+  while (i != 4) {
+    if (integer <= 0)
       dataArray[i] = 0;
-      
-    dataArray[i] = data % 10;
-    data /= 10;
+
+    dataArray[i] = integer % 10;
+    integer /= 10;
+    i++;
+  }
+  while (j != 8) {
+    if (decimal <= 0)
+      dataArray[j] = 0;
+
+    dataArray[j] = decimal % 10;
+    decimal /= 10;
+    j++;
   }
 }
 
@@ -58,8 +84,11 @@ int counter = 0;
 uint16_t selector = 1;
 
 void reciveEvent(int howMany) {
-  if (howMany >= (sizeof i2c_data)) {
-    I2C_readAnything (i2c_data);
+  if (howMany >= (sizeof decimal)) {
+    I2C_readAnything (decimal);
+    integer=decimal;
+    decimal-=integer;
+    decimal*=10000;
     //    Serial.print("in i2c");
     //    Serial.println(i2c_data);
   }
@@ -71,13 +100,16 @@ void loop() {
   counter++;
   counter %= 8;
   selector %= 255;
-  dataToArray(i2c_data);
+  dataToArray(integer, decimal);
   delay(1);
 }
 void disp(uint8_t digit = 0, uint8_t place = 0) {
   digitalWrite(LOAD, LOW);
   SPI.setBitOrder(MSBFIRST);
   SPI.transfer(place);//transfer place
-  SPI.transfer(dg[digit]);//transfer digit
+  if (counter == 4)
+    SPI.transfer(dg1[digit]);//transfer digit
+  else
+    SPI.transfer(dg[digit]);//transfer digit
   digitalWrite(LOAD, HIGH);
 }
